aa8891b4e9820a67a85dfcacb2912da76f5b4bb7
WorkPC <svitovan@outlook.com>
2023-02-20 16:53:19 +0100

    upload

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '23a7487151msh35dbcfda64163a5p15364bjsnb93a1d5f8fd2',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+
+export const WEATHER_API_KEY = 'f642c2ea8fb61bd7c1a5730a56b8ddb6';
