eeb29d845d62be72ef15d4438b5329523ec99744
WorkPC <svitovan@outlook.com>
2023-02-22 14:47:13 +0100

    final

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import CurrentWeather from "./components/current-weather/current-weather";
 import { WEATHER_API_KEY, WEATHER_API_URL } from "./api";
 import { useState } from "react";
+import Forecast from "./components/forecast/forecast";
 
 function App() {
   const [currentWeather, setCurrentWeather] = useState(null);
@@ -30,6 +31,7 @@ function App() {
     <div className="container">
       <Search onSearchChange={handleOnSearchChange} />
       {currentWeather && <CurrentWeather data={currentWeather}/>}
+      {forecast && <Forecast data={forecast}/>}
     </div>
   );
 }
